9b9bee92aff979ab55f1dfaf396c85c50c3e3111
Jd Fiscus <me@iamfiscus.com>
2023-04-04 19:29:38 -0400

    initial commit

--- _examples/4-lsp.js
+++ _examples/4-lsp.js
@@ -1,29 +1,7 @@
 // Meal superclass
-class Meal {
-  prepare() {}
-}
 
 // Salad subclass extending the Meal superclass
-class Salad extends Meal {
-  prepare() {
-    // code for preparing a salad
-  }
-}
 
 // Pizza subclass extending the Meal superclass
-class Pizza extends Meal {
-  prepare() {
-    // code for preparing a pizza
-  }
-}
 
 // Ensuring LSP by using any Meal subclass interchangeably
-function prepareMeal(meal) {
-  meal.prepare();
-}
-
-const salad = new Salad();
-const pizza = new Pizza();
-
-prepareMeal(salad); // outputs "Preparing salad"
-prepareMeal(pizza); // outputs "Preparing pizza"
